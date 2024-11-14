940d959f44dc829bf8261d8224de40176847fec3
likagit1 <zalikatraore3m@gmail.com>
2024-11-14 09:56:52 +0000

    sauvergarde des modifs

---
+++ src/app/presentation/features/space-owner/presentation/layout/list-bien-owner/list-bien-owner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-bien-owner',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-bien-owner.component.html',
+  styleUrl: './list-bien-owner.component.scss'
+})
+export class ListBienOwnerComponent {
+
+}
