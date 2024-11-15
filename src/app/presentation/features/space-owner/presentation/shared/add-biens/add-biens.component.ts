977bd9f8440ee59e84c5fb038e2e6f4f10893bcd
juniorAsaki <jncho1717@gmail.com>
2024-11-15 17:30:00 +0100

    #9-mockedatafortheresidence : ajout de l'interface residence et creation d'une liste de bien avec les données mocké

--- src/app/presentation/features/space-owner/presentation/shared/add-biens/add-biens.component.ts
+++ src/app/presentation/features/space-owner/presentation/shared/add-biens/add-biens.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-add-biens',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './add-biens.component.html',
   styleUrl: './add-biens.component.scss'
 })
